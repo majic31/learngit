d709960dc050b4bf680c38411c03dd94a999abb1
majic31 <majic31@163.com>
2015-11-25 00:35:18 +0800

    add some other function

--- test.cpp
+++ test.cpp
@@ -3,6 +3,8 @@
 int main(int argc, char * argv[] ){
 	printf( "HELLO WORLD\n" );
 	// must return
-	printf( "must do something here" );
+	printf( "must do something here\n" );
+	printf( "please work" );
 	return 1;
 }
+// add some other function
